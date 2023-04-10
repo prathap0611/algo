e73b0cd7d8b7c32a941afae7dc335b4f5153d56f
Hari Prathap <hari.prathap@cloudsensesolutions.com>
2023-04-10 19:31:41 +0530

    Update countSort algorithm to retain stability

--- countingSort.js
+++ countingSort.js
@@ -1,3 +1,8 @@
+// 1. Given k, initialize an array of k elements with 0
+// 2. Iterate through array and update countArr
+// 3. Iterate through countArr to calculate countArr position which will help to place the actual arr elements in appropriate position
+// 4. Iterate through arr and place the elements as per countArrPosition
+
 function countingSort(arr, k) {
     let initialArr = Array.from({length: k+1}).fill(0)
     countArr = arr.reduce((countArr, ele) => {
@@ -5,10 +10,22 @@ function countingSort(arr, k) {
         return countArr;
     }, initialArr);
 
-    return countArr.reduce((result, ele, index) => {
-        result = [...result, ...Array.from({length: ele}).fill(index)]
-        return result;
-    }, []);
+    let countArrPosition = countArr.reduce((arr, ele, index) => {
+        if(index > 0) {
+            arr[index] = ele + arr[index - 1];
+        }
+        return arr
+    }, countArr);
+
+    let returnVal = [];
+
+    for(let i = arr.length-1; i>=0; i--) {
+        const ele = arr[i];
+        countArrPosition[ele] = countArrPosition[ele] - 1
+        returnVal[countArrPosition[ele]] = arr[i]
+    }
+
+    return returnVal;
 }
 
 (() => {
